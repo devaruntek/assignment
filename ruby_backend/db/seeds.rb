9bf69d38b221362dff3075f6775dd58e9783154e
Arun Tekwani <aruntekwani106@gmail.com>
2024-02-13 11:02:59 +0530

    initial commit

--- ruby_backend/db/seeds.rb
+++ ruby_backend/db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(email: "admin@yopmail.com", password: "Test@1234") unless User.find_by_email("admin@yopmail.com")
+Customer.find_or_create_by(email: "abc@yopmail.com", name: "Abc", mobile_number: "9212345321", age: 25)
